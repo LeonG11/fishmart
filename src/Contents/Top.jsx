f2e55297ccd2218ffc1e9a7446c6263011dda145
LeonG11 <kkacanov198@gmail.com>
2022-07-20 18:38:35 +0300

    Render site

--- src/Contents/Top.jsx
+++ src/Contents/Top.jsx
@@ -8,53 +8,78 @@ function Dot() {
 
 function TopLink() {
     return (
-        <div className="header__block">
-            <Link to="/" className="header__block__a">
-                <div className="header__block__container">
-                    <MediaQuery minWidth={768}>
-                        <img src="./img/logo.svg" alt="FISH FISH"/>
-                        <div>FISH FISH</div>
-                    </MediaQuery>
-                </div>
-            </Link>
-            <div className="navlink">
-                <MediaQuery minWidth={768}>
-                    <Link to="/fishfish">
-                        <div className="fm">
-                            FISH FISH
+        <>
+            <MediaQuery minWidth={768}>
+                <div className="header__block">
+                    <Link to="/" className="header__block__a">
+                        <div className="header__block__container">
+                            <img src="./img/logo.svg" alt="FISH FISH"/>
+                            <div>FISH FISH</div>
                         </div>
                     </Link>
-                    <Dot/>
-                    <Link to="/menu">
-                        <div className="fm">
-                            АССОРТИМЕНТ
-                        </div>
-                    </Link>
-                    <Dot/>
-                    <Link to="/about">
-                        <div className="fm">
-                            КОНТАКТЫ
-                        </div>
-                    </Link>
-                </MediaQuery>
-                <MediaQuery maxWidth={768}>
-                    <select className='w-full'>
-                        <option>FISH FISH</option>
-                        <option>АССОРТИМЕНТ</option>
-                        <option>КОНТАКТЫ</option>
-                    </select>
-                </MediaQuery>
 
-            </div>
-            <div className="col-start-12 items-center flex justify-right">
-                <button className="fm">
-                    <img className="w-6 h-6 invert mr-7" src="./img/user.svg" alt=""/>
-                </button>
-                <button className="fm">
-                    <img className="w-6 h-6 invert" src="./img/basket.svg" alt=""/>
-                </button>
-            </div>
-        </div>
+                    <div className="nav__link">
+                        <Link to="/fishfish">
+                            <div className="fm">
+                                FISH FISH
+                            </div>
+                        </Link>
+                        <Dot/>
+                        <Link to="/menu">
+                            <div className="fm">
+                                АССОРТИМЕНТ
+                            </div>
+                        </Link>
+                        <Dot/>
+                        <Link to="/about">
+                            <div className="fm">
+                                КОНТАКТЫ
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="col-start-12 items-center flex justify-right">
+                        <button className="fm">
+                            <img className="w-6 h-6 invert mr-7" src="./img/user.svg" alt=""/>
+                        </button>
+                        <button className="fm">
+                            <img className="w-6 h-6 invert" src="./img/basket.svg" alt=""/>
+                        </button>
+                    </div>
+                </div>
+            </MediaQuery>
+            <MediaQuery maxWidth={768}>
+                <div className="header__block">
+                    <div hidden={false} className="burger__menu">
+                        <Link to="/fishfish">
+                            <div>
+                                FISH FISH
+                            </div>
+                        </Link>
+                        <Link to="/menu">
+                            <div>
+                                АССОРТИМЕНТ
+                            </div>
+                        </Link>
+                        <Link to="/about">
+                            <div>
+                                КОНТАКТЫ
+                            </div>
+                        </Link>
+                    </div>
+                    <button className="burger">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </button>
+                    <Link to="/" className="header__block__container">
+                        <div>
+                            <img src="./img/logo.svg" alt="FISH FISH"/>
+                            <div>FISH FISH</div>
+                        </div>
+                    </Link>
+                </div>
+            </MediaQuery>
+        </>
     );
 }
 
