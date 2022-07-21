0b5f5df710301b7a7a104e3336223d791a0fc4ff
LeonG11 <kkacanov198@gmail.com>
2022-07-21 11:04:39 +0300

    Make burger menu

--- src/Components/MobileNav.jsx
+++ src/Components/MobileNav.jsx
@@ -1,71 +1,54 @@
 import MediaQuery from "react-responsive";
 import {Link} from "react-router-dom";
-import React, {useState} from "react";
+import React from "react";
+import {slide as Menu} from 'react-burger-menu'
 
 export function MobileNav() {
 
-    const [open, setOpen] = useState(false);
-
-
-    const invertOpen = () => {
-        if (open) {
-            setOpen(false)
-        } else {
-            setOpen(true)
-
-        }
-    }
-
-
     return <MediaQuery maxWidth={768}>
-        <div className="header__block">
-            {/*<Menu>*/}
-            {/*    <a className=>1</a>*/}
-            {/*    <a className=>2</a>*/}
-            {/*    <a className=>3</a>*/}
-            {/*    <a className=>4</a>*/}
-            {/*    <a className=>5</a>*/}
-            {/*</Menu>*/}
-            <div className="z-10 absolute menu" hidden={open} style={{
-                backgroundColor: '#051B26',
-                width: '25%',
-                height: '100vh',
-                top: '0',
-                marginLeft: '-20px',
-                textAlign: 'left'
-            }}>
-                <button style={{
-                    alignSelf: 'end'
-                }} onClick={invertOpen}>
-                    X
-                </button>
-                <Link to="/fishfish">
-                    <div className="menu__button">
-                        FISH FISH
-                    </div>
-                </Link>
-                <Link to="/menu">
-                    <div className="menu__button">
-                        АССОРТИМЕНТ
-                    </div>
-                </Link>
-                <Link to="/about">
-                    <div className="menu__button">
-                        КОНТАКТЫ
-                    </div>
-                </Link>
-            </div>
-            <button className="burger" onClick={invertOpen}>
-                <div></div>
-                <div></div>
-                <div></div>
-            </button>
-            <Link to="/" className="header__block__container">
-                <div>
-                    <img src="./img/logo.svg" alt="FISH FISH"/>
-                    <div>FISH FISH</div>
+        <>
+            <div id="outer-container">
+                <Menu pageWrapId={"page-wrap"} className="text-white bg-[#051B26] p-3"
+                      outerContainerId={"outer-container"} width={'30%'}>
+                    <main id="page-wrap">
+                        <ul className="text-center ul_burger">
+                            <li>
+                                <Link to="/fishfish">
+                                    FISH FISH
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/menu">
+                                    АССОРТИМЕНТ
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/about">
+                                    КОНТАКТЫ
+                                </Link>
+                            </li>
+                        </ul>
+                    </main>
+                </Menu>
+                <div className="header__block">
+
+                    <button className="burger" id="react-burger-menu-btn">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </button>
+                    <Link to="/" className="">
+                        <div>
+                            <img src="./img/logo.svg" alt="FISH FISH"/>
+                            <div style={{
+                                fontSize: '15px'
+                            }}>FISH FISH
+                            </div>
+                        </div>
+                    </Link>
                 </div>
-            </Link>
-        </div>
-    </MediaQuery>;
+            </div>
+        </>
+    </MediaQuery>
+        ;
 }
